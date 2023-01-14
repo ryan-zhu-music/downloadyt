0e2eadf120405889c418506844e578968c165bc7
RZ-Music <ryanzhu2005@gmail.com>
2023-01-14 16:08:54 -0500

    loading, responsive design, footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import { Inter } from "@next/font/google";
+import { useEffect, useState, CSSProperties } from "react";
 import { ImNewTab, ImSearch } from "react-icons/im";
+import { RingLoader } from "react-spinners";
 
-const inter = Inter({ subsets: ["latin"] });
+const override: CSSProperties = {
+  display: "block",
+  margin: "14px auto",
+  borderColor: "red",
+};
 
 export default function Home() {
   const [link, setLink] = useState("");
@@ -12,6 +16,7 @@ export default function Home() {
   const [audio, setAudio] = useState<any>(null);
   const [allFormats, setAllFormats] = useState<any>(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const [videoType, setVideoType] = useState({
     quality: "",
     format: "video/mp4",
@@ -37,8 +42,7 @@ export default function Home() {
       .then((res) => res.json())
       .then((data) => {
         setLoading(false);
-        console.log(data);
-
+        setError("");
         setMetadata(data.response.videoDetails);
         setAllFormats(data.response.streamingData.adaptiveFormats);
         const defaultVideo = data.response.streamingData.adaptiveFormats.find(
@@ -59,10 +63,12 @@ export default function Home() {
       })
       .catch((err) => {
         console.log("error", err);
+        setLoading(false);
+        setError(
+          "Something went wrong. Please try again later, or try a different video."
+        );
       });
   };
-
-  console.log("video", video, video?.qualityLabel);
 
   useEffect(() => {
     setVideo(
@@ -95,19 +101,20 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="w-screen min-h-screen flex flex-col items-center justify-center bg-[url('/background.png')] bg-cover bg-center px-5 md:px-16 lg:px-32 py-32">
-        <h1 className="text-7xl font-bold gradient-red text-transparent !bg-clip-text uppercase text-center">
+      <main className="w-screen min-h-screen flex flex-col items-center justify-center bg-[url('/background.png')] bg-cover bg-center px-10 md:px-20 lg:px-32 pt-32">
+        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold gradient-red text-transparent !bg-clip-text uppercase text-center">
           YouTube Downloader
         </h1>
-        <h2 className="text-2xl font-medium text-white text-center">
-          Download any YouTube video in video (mp4) or audio (m4a) format!
+        <h2 className="text-xl md:text-2xl font-medium text-white text-center">
+          Download any YouTube video in video (mp4) or audio (m4a) format
+          instantly!
         </h2>
         <div className="w-5/6 md:w-[550px] h-14 mt-5 rounded-full gradient-red p-[3px]">
           <div className="w-full h-full rounded-full flex items-stretch justify-between flex-nowrap">
             <input
               type="text"
               placeholder="e.g. https://www.youtube.com/watch?v=fs-eur71DRA"
-              className="w-full text-white text-lg rounded-l-full pl-4 font-normal focus:outline-none placeholder:italic placeholder:text-light-red bg-dark-red focus:medium-red hover:medium-red duration-500"
+              className="w-full text-white text-base md:text-lg rounded-l-full pl-4 font-normal focus:outline-none placeholder:italic placeholder:text-light-red bg-dark-red focus:medium-red hover:medium-red duration-500"
               onChange={(e) => setLink(e.target.value.trim())}
               onKeyDown={(e) => {
                 if (e.key == "Enter") {
@@ -124,159 +131,180 @@ export default function Home() {
             </button>
           </div>
         </div>
+        {error && (
+          <p className="w-5/6 md:w-[550px] h-14 mt-5 text-red-400 text-center">
+            {error}
+          </p>
+        )}
+        <RingLoader
+          color={"#B93437"}
+          loading={loading}
+          size={100}
+          cssOverride={override}
+        />
         <div
           className={
             "w-full mt-10 grid md:grid-cols-2 md:grid-rows-1 gap-10 duration-500 ease-in-out " +
-            (!loading && !(video && audio && metadata)
+            (!(video && audio && metadata)
               ? "max-h-0 overflow-y-hidden"
               : "max-h-[1000px]")
           }
         >
           <div className="w-full p-2 gradient-red">
             <div className="w-full h-full bg-dark-red p-5">
-              <h3 className="text-2xl font-bold text-white">
-                {metadata?.title}
-              </h3>
-              <div className="h-1 w-20 bg-white opacity-50 my-4"></div>
-              <ul className="text-lg text-white">
-                <li className="flex">
-                  Channel:
-                  <a
-                    href={
-                      "https://www.youtube.com/channel/" + metadata?.channelId
-                    }
-                    className="ml-1 text-lg text-light-red underline flex items-center justify-start"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    {metadata?.author}
-                    <ImNewTab className="inline-block ml-1 mb-1" />
-                  </a>
-                </li>
-                <li>
-                  Views:{" "}
-                  <span className="text-light-red">{metadata?.viewCount}</span>
-                </li>
-                <li>
-                  Duration:{" "}
-                  <span className="text-light-red">
-                    {parseSeconds(metadata?.lengthSeconds)}
-                  </span>
-                </li>
-                <li>
-                  Video quality:{" "}
-                  <select
-                    className="text-light-red rounded ml-2 p-2 bg-dark-red"
-                    onChange={(e) => {
-                      setVideo(null);
-                      setVideoType({
-                        quality: e.target.value,
-                        format: videoType.format,
-                      });
-                    }}
-                  >
-                    {allFormats?.map((format: any, index: number) => {
-                      if (
-                        format.mimeType.includes("video/mp4") &&
-                        !qualities.includes(format.quality)
-                      ) {
-                        qualities.push(format.quality);
-                        return (
-                          <option key={index} className="p-1">
-                            {format.qualityLabel}
-                          </option>
-                        );
-                      }
-                    })}
-                  </select>
-                </li>
-                <li>
-                  Video format:{" "}
-                  <form className="flex flex-col justify-center items-start">
-                    <div className="flex">
-                      <input
-                        className="ml-6"
-                        type="radio"
-                        name="Video format"
-                        id="mp4"
-                        value="mp4"
-                        onChange={() => {
+              {metadata && (
+                <>
+                  <h3 className="text-lg md:text-xl lg:text-2xl font-bold text-white">
+                    {metadata?.title}
+                  </h3>
+                  <div className="h-1 w-20 bg-white opacity-50 my-4"></div>
+                  <ul className="text-base md:text-lg text-white">
+                    <li className="flex">
+                      Channel:
+                      <a
+                        href={
+                          "https://www.youtube.com/channel/" +
+                          metadata?.channelId
+                        }
+                        className="ml-1 text-base md:text-lg text-light-red underline flex items-center justify-start"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                      >
+                        {metadata?.author}
+                        <ImNewTab className="inline-block ml-1 mb-1" />
+                      </a>
+                    </li>
+                    <li>
+                      Views:{" "}
+                      <span className="text-light-red">
+                        {metadata?.viewCount}
+                      </span>
+                    </li>
+                    <li>
+                      Duration:{" "}
+                      <span className="text-light-red">
+                        {parseSeconds(metadata?.lengthSeconds)}
+                      </span>
+                    </li>
+                    <li>
+                      Video quality:{" "}
+                      <select
+                        className="text-light-red rounded ml-2 p-2 bg-dark-red"
+                        onChange={(e) => {
                           setVideo(null);
                           setVideoType({
-                            quality: videoType.quality,
-                            format: "video/mp4",
+                            quality: e.target.value,
+                            format: videoType.format,
                           });
                         }}
-                      />
-                      <label className="ml-1" htmlFor="mp4">
-                        mp4
-                      </label>
-                    </div>
-                    <div className="flex">
-                      <input
-                        className="ml-6"
-                        type="radio"
-                        name="format"
-                        id="webm"
-                        value="webm"
-                        onChange={() => {
-                          setVideo(null);
-                          setVideoType({
-                            quality: videoType.quality,
-                            format: "video/webm",
-                          });
-                        }}
-                      />
-                      <label className="ml-1" htmlFor="webm">
-                        webm
-                      </label>
-                    </div>
-                  </form>
-                </li>
-                <li>
-                  Audio format:{" "}
-                  <form className="flex flex-col justify-center items-start">
-                    <div className="flex">
-                      <input
-                        className="ml-6"
-                        type="radio"
-                        name="Audio format"
-                        id="m4a"
-                        value="m4a"
-                        onChange={() => {
-                          setAudio(null);
-                          setAudioType("audio/mp4");
-                        }}
-                      />
-                      <label className="ml-1" htmlFor="m4a">
-                        m4a
-                      </label>
-                    </div>
-                    <div className="flex">
-                      <input
-                        className="ml-6"
-                        type="radio"
-                        name="format"
-                        id="weba"
-                        value="weba"
-                        onChange={() => {
-                          setAudio(null);
-                          setAudioType("audio/webm");
-                        }}
-                      />
-                      <label className="ml-1" htmlFor="webm">
-                        webm
-                      </label>
-                    </div>
-                  </form>
-                </li>
-              </ul>
+                      >
+                        {allFormats?.map((format: any, index: number) => {
+                          if (
+                            format.mimeType.includes("video/mp4") &&
+                            !qualities.includes(format.quality)
+                          ) {
+                            qualities.push(format.quality);
+                            return (
+                              <option key={index} className="p-1">
+                                {format.qualityLabel}
+                              </option>
+                            );
+                          }
+                        })}
+                      </select>
+                    </li>
+                    <li>
+                      Video format:{" "}
+                      <form className="flex flex-col justify-center items-start">
+                        <div className="flex">
+                          <input
+                            className="ml-6"
+                            type="radio"
+                            name="Video format"
+                            id="mp4"
+                            value="mp4"
+                            onChange={() => {
+                              setVideo(null);
+                              setVideoType({
+                                quality: videoType.quality,
+                                format: "video/mp4",
+                              });
+                            }}
+                          />
+                          <label className="ml-1" htmlFor="mp4">
+                            mp4
+                          </label>
+                        </div>
+                        <div className="flex">
+                          <input
+                            className="ml-6"
+                            type="radio"
+                            name="format"
+                            id="webm"
+                            value="webm"
+                            onChange={() => {
+                              setVideo(null);
+                              setVideoType({
+                                quality: videoType.quality,
+                                format: "video/webm",
+                              });
+                            }}
+                          />
+                          <label className="ml-1" htmlFor="webm">
+                            webm
+                          </label>
+                        </div>
+                      </form>
+                    </li>
+                    <li>
+                      Audio format:{" "}
+                      <form className="flex flex-col justify-center items-start">
+                        <div className="flex">
+                          <input
+                            className="ml-6"
+                            type="radio"
+                            name="Audio format"
+                            id="m4a"
+                            value="m4a"
+                            onChange={() => {
+                              setAudio(null);
+                              setAudioType("audio/mp4");
+                            }}
+                          />
+                          <label className="ml-1" htmlFor="m4a">
+                            m4a
+                          </label>
+                        </div>
+                        <div className="flex">
+                          <input
+                            className="ml-6"
+                            type="radio"
+                            name="format"
+                            id="weba"
+                            value="weba"
+                            onChange={() => {
+                              setAudio(null);
+                              setAudioType("audio/webm");
+                            }}
+                          />
+                          <label className="ml-1" htmlFor="webm">
+                            webm
+                          </label>
+                        </div>
+                      </form>
+                    </li>
+                  </ul>
+                  <p className="text-sm md:text-base text-light-red mt-10">
+                    Hint: click the three dots (⋮) on the right to download!
+                  </p>
+                </>
+              )}
             </div>
           </div>
           <div className="w-full">
             {video && (
               <div className="flex flex-col mb-7">
-                <h4 className="text-2xl font-semibold text-white mb-1">
+                <h4 className="text-xl md:text-2xl font-semibold text-white mb-1">
                   MP4 Video ({video.width}x{video.height})
                 </h4>
                 <div className="p-2 gradient-red">
@@ -288,7 +316,7 @@ export default function Home() {
             )}
             {audio && (
               <div className="flex flex-col">
-                <h4 className="text-2xl font-semibold text-white mb-1">
+                <h4 className="text-xl md:text-2xl font-semibold text-white mb-1">
                   M4A Audio
                 </h4>
                 <div className="p-2 gradient-red rounded-full">
@@ -300,6 +328,30 @@ export default function Home() {
             )}
           </div>
         </div>
+        <footer className="w-full py-16 flex flex-col justify-center items-center">
+          <p className="text-light-red text-base md:text-md">
+            View{" "}
+            <a
+              href="https://github.com/ryan-zhu-music/youtube-downloader"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="font-bold underline hover:text-red-500/50 duration-500"
+            >
+              GitHub repo
+            </a>
+          </p>
+          <p className="text-light-red text-base md:text-md">
+            Made with Next.js by{" "}
+            <a
+              href="https://www.ryanzhu.com"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="font-bold underline hover:text-red-500/50 duration-500"
+            >
+              Ryan Zhu
+            </a>
+          </p>
+        </footer>
       </main>
     </>
   );
